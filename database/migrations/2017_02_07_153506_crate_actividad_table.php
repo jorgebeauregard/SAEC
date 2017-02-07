096aa8fa2da2800ae4cdfc28882ba6c65de74dae
Eduardo Luna <Lalo@MacBook-Pro-de-Eduardo-7.local>
2017-02-07 12:10:25 -0600

    Tablas y modificacion de el modelo

--- database/migrations/2017_02_07_153506_crate_actividad_table.php
+++ database/migrations/2017_02_07_153506_crate_actividad_table.php
@@ -15,6 +15,10 @@ class CrateActividadTable extends Migration
     {
         Schema::create('actividad', function (Blueprint $table) {
             $table->increments('id');
+            $table->String('nombre', 60);
+            $table->String('description', 255);
+            $table->integer('id_crn')->unsigned();
+            $table->foreign('id_crn')->references('id')->on('crn')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
     }
