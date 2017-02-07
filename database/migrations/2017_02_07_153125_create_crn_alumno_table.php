4b24b98196c79ed7c37bda00f4e034c5d1eefcb2
Jorge Beauregard <Jorge Beauregard>
2017-02-07 12:13:00 -0600

    Se arreglaron algunos typos y se agrego la tabla de crn_alumno

--- database/migrations/2017_02_07_153125_create_crn_alumno_table.php
+++ database/migrations/2017_02_07_153125_create_crn_alumno_table.php
@@ -15,6 +15,11 @@ class CreateCrnAlumnoTable extends Migration
     {
         Schema::create('crn_alumno', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('id_alumno')->unsigned();
+            $table->integer('id_crn')->unsigned();
+            $table->integer('faltas');
+            $table->foreign('id_alumno')->references('id')->on('alumno')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('id_crn')->references('id')->on('crn')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
     }
