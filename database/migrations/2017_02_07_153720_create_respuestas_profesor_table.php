a143ebfaa84791ccf91af030bab7b3d812738e45
Jorge Beauregard <Jorge Beauregard>
2017-02-10 09:09:23 -0600

    Se borro la tabla crate

--- database/migrations/2017_02_07_153720_create_respuestas_profesor_table.php
+++ database/migrations/2017_02_07_153720_create_respuestas_profesor_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CrateActividadTable extends Migration
+class CreateRespuestasProfesorTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,8 @@ class CrateActividadTable extends Migration
      */
     public function up()
     {
-        Schema::create('actividad', function (Blueprint $table) {
+        Schema::create('respuestas_profesor', function (Blueprint $table) {
             $table->increments('id');
-            $table->String('nombre', 60);
-            $table->String('description', 255);
-            $table->integer('id_crn')->unsigned();
-            $table->foreign('id_crn')->references('id')->on('crn')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
     }
@@ -30,6 +26,6 @@ class CrateActividadTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('actividad');
+        Schema::dropIfExists('respuestas_profesor');
     }
 }
