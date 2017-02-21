dcf8323c38cedd308a7ae6f70c6116558f228dc8
Jorge Beauregard <Jorge Beauregard>
2017-02-21 10:23:57 -0600

    Colores y perfil de usuario

--- resources/views/perfil/index.blade.php
+++ resources/views/perfil/index.blade.php
@@ -9,4 +9,40 @@
 
 @section('description', 'Esta es la pagina de compras')
 @section('content')
+			<div class="content">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-lg-12 col-md-12 col-sm-12">
+							<div class="card card-stats">
+								<div class="card-header" data-background-color="blue">
+									<i class="material-icons">face</i>
+								</div>
+								<div class="card-content">
+									<br>
+									<br>
+									<br>
+									<br>
+									<p class="category" >Usuario</p>
+									<h3 class="title">Julián Beauregard Luna</h3>
+									<br>
+									<p class="category">Matr&iacutecula</p>
+									<h3 class="title">A01328439</h3>
+									<br>
+									<p class="category">Campus</p>
+									<h3 class="title">Puebla</h3>
+									<br>
+									<p class="category">Programa</p>
+									<h3 class="title">ITC11</h3>																												
+								</div>
+								<div class="card-footer">
+
+								</div>
+							</div>
+						</div>
+					</div>
+
+					</div>
+				</div>
+			</div>
+
 @endsection
