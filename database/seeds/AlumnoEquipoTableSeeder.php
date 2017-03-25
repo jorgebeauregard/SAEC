7726f55ade8c5588fcebaf1325136029dab01a31
Angel Roberto Ruiz Mendoza <Angel Roberto Ruiz Mendoza>
2017-03-25 11:30:08 -0600

    Seeder de alumno corregido y AlumnoEquipo terminado

--- database/seeds/AlumnoEquipoTableSeeder.php
+++ database/seeds/AlumnoEquipoTableSeeder.php
@@ -17,13 +17,547 @@ class AlumnoEquipoTableSeeder extends Seeder
         ]);
 
         DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 1,
+        	'alumno_id' => 2,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 1,
+        	'alumno_id' => 3,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 1,
+        	'alumno_id' => 4,
+        ]);
+
+        //
+
+         DB::table('alumno_equipo')->insert([
         	'equipo_id' => 2,
-        	'alumno_id' => 1,
+        	'alumno_id' => 5,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 2,
+        	'alumno_id' => 6,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 2,
+        	'alumno_id' => 7,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 2,
+        	'alumno_id' => 8,
+        ]);
+
+        //
+
+         DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 3,
+        	'alumno_id' => 9,
         ]);
 
         DB::table('alumno_equipo')->insert([
         	'equipo_id' => 3,
+        	'alumno_id' => 10,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 3,
+        	'alumno_id' => 11,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 3,
+        	'alumno_id' => 12,
+        ]);
+
+        //
+
+         DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 4,
+        	'alumno_id' => 13,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 4,
+        	'alumno_id' => 14,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 4,
+        	'alumno_id' => 15,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 4,
+        	'alumno_id' => 16,
+        ]);
+
+        //
+
+         DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 5,
+        	'alumno_id' => 17,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 5,
+        	'alumno_id' => 18,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 5,
+        	'alumno_id' => 19,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 5,
+        	'alumno_id' => 20,
+        ]);
+
+        //
+
+         DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 6,
+        	'alumno_id' => 21,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 6,
+        	'alumno_id' => 22,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 6,
+        	'alumno_id' => 23,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 6,
+        	'alumno_id' => 24,
+        ]);
+
+        //
+
+         DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 7,
+        	'alumno_id' => 25,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 7,
+        	'alumno_id' => 26,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 7,
+        	'alumno_id' => 27,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 7,
+        	'alumno_id' => 28,
+        ]);
+
+        //
+
+         DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 8,
+        	'alumno_id' => 29,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 8,
+        	'alumno_id' => 30,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 8,
+        	'alumno_id' => 31,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 8,
+        	'alumno_id' => 32,
+        ]);
+
+        //
+
+         DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 9,
+        	'alumno_id' => 33,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 9,
+        	'alumno_id' => 34,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 9,
+        	'alumno_id' => 35,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 9,
+        	'alumno_id' => 36,
+        ]);
+
+        //
+
+         DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 10,
+        	'alumno_id' => 37,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 10,
+        	'alumno_id' => 38,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 10,
+        	'alumno_id' => 39,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 10,
+        	'alumno_id' => 40,
+        ]);
+
+        //
+
+         DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 11,
+        	'alumno_id' => 41,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 11,
+        	'alumno_id' => 42,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 11,
+        	'alumno_id' => 43,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 11,
+        	'alumno_id' => 44,
+        ]);
+
+        //
+
+         DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 12,
+        	'alumno_id' => 45,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 12,
+        	'alumno_id' => 46,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 12,
+        	'alumno_id' => 47,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 12,
+        	'alumno_id' => 48,
+        ]);
+
+        //
+
+         DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 13,
+        	'alumno_id' => 49,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 13,
+        	'alumno_id' => 50,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 13,
         	'alumno_id' => 1,
         ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 13,
+        	'alumno_id' => 2,
+        ]);
+
+        //
+
+         DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 14,
+        	'alumno_id' => 3,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 14,
+        	'alumno_id' => 4,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 14,
+        	'alumno_id' => 5,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 14,
+        	'alumno_id' => 6,
+        ]);
+
+        //
+
+         DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 15,
+        	'alumno_id' => 7,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 15,
+        	'alumno_id' => 8,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 15,
+        	'alumno_id' => 9,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 15,
+        	'alumno_id' => 10,
+        ]);
+
+        //
+
+         DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 16,
+        	'alumno_id' => 11,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 16,
+        	'alumno_id' => 12,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 16,
+        	'alumno_id' => 13,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 16,
+        	'alumno_id' => 14,
+        ]);
+
+        //
+
+         DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 17,
+        	'alumno_id' => 15,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 17,
+        	'alumno_id' => 16,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 17,
+        	'alumno_id' => 17,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 17,
+        	'alumno_id' => 18,
+        ]);
+
+        //
+
+         DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 18,
+        	'alumno_id' => 19,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 18,
+        	'alumno_id' => 20,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 18,
+        	'alumno_id' => 21,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 18,
+        	'alumno_id' => 22,
+        ]);
+
+        //
+
+         DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 19,
+        	'alumno_id' => 23,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 19,
+        	'alumno_id' => 24,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 19,
+        	'alumno_id' => 25,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 19,
+        	'alumno_id' => 26,
+        ]);
+
+        //
+
+         DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 20,
+        	'alumno_id' => 27,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 20,
+        	'alumno_id' => 28,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 20,
+        	'alumno_id' => 29,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 20,
+        	'alumno_id' => 30,
+        ]);
+
+        //
+
+         DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 21,
+        	'alumno_id' => 31,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 21,
+        	'alumno_id' => 32,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 21,
+        	'alumno_id' => 33,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 21,
+        	'alumno_id' => 34,
+        ]);
+
+        //
+
+         DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 22,
+        	'alumno_id' => 35,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 22,
+        	'alumno_id' => 36,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 22,
+        	'alumno_id' => 37,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 22,
+        	'alumno_id' => 38,
+        ]);
+
+        //
+
+         DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 23,
+        	'alumno_id' => 39,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 23,
+        	'alumno_id' => 40,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 23,
+        	'alumno_id' => 41,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 23,
+        	'alumno_id' => 42,
+        ]);
+
+        //
+
+         DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 24,
+        	'alumno_id' => 43,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 24,
+        	'alumno_id' => 44,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 24,
+        	'alumno_id' => 45,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 24,
+        	'alumno_id' => 46,
+        ]);
+
+        //
+
+         DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 25,
+        	'alumno_id' => 47,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 25,
+        	'alumno_id' => 48,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 25,
+        	'alumno_id' => 49,
+        ]);
+
+        DB::table('alumno_equipo')->insert([
+        	'equipo_id' => 25,
+        	'alumno_id' => 50,
+        ]);
+
     }
 }
