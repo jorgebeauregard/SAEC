627d0d6a15ac75dfc22d23713c74bd99bcd7a43e
Angel Roberto Ruiz Mendoza <Angel Roberto Ruiz Mendoza>
2017-03-25 12:59:04 -0600

    Seeders de equipo otra vez

--- database/seeds/EquiposTableSeeder.php
+++ database/seeds/EquiposTableSeeder.php
@@ -16,14 +16,124 @@ class EquiposTableSeeder extends Seeder
         	'actividad_id' => 1,
         ]);
 
-        DB::table('equipos')->insert([
-        	'numero_equipo' => 3,
-        	'actividad_id' => 2,
-        ]);
-
-        DB::table('equipos')->insert([
-        	'numero_equipo' => 4,
-        	'actividad_id' => 3,
-        ]);
+         DB::table('equipos')->insert([
+          	'numero_equipo' => 2,
+          	'actividad_id' => 1,
+          ]);
+  
+          DB::table('equipos')->insert([
+          	'numero_equipo' => 3,
+          	'actividad_id' => 1,
+          ]);
+  
+          DB::table('equipos')->insert([
+          	'numero_equipo' => 4,
+          	'actividad_id' => 1,
+          ]);
+  
+          DB::table('equipos')->insert([
+          	'numero_equipo' => 5,
+          	'actividad_id' => 1,
+          ]);
+  
+          DB::table('equipos')->insert([
+          	'numero_equipo' => 1,
+          	'actividad_id' => 2,
+          ]);
+  
+          DB::table('equipos')->insert([
+          	'numero_equipo' => 2,
+          	'actividad_id' => 2,
+          ]);
+  
+          DB::table('equipos')->insert([
+          	'numero_equipo' => 3,
+          	'actividad_id' => 2,
+          ]);
+  
+          DB::table('equipos')->insert([
+          	'numero_equipo' => 4,
+          	'actividad_id' => 2,
+          ]);
+  
+          DB::table('equipos')->insert([
+          	'numero_equipo' => 5,
+          	'actividad_id' => 2,
+          ]);
+  
+          DB::table('equipos')->insert([
+          	'numero_equipo' => 1,
+          	'actividad_id' => 3,
+          ]);
+  
+          DB::table('equipos')->insert([
+          	'numero_equipo' => 2,
+          	'actividad_id' => 3,
+          ]);
+  
+          DB::table('equipos')->insert([
+          	'numero_equipo' => 3,
+          	'actividad_id' => 3,
+          ]);
+  
+          DB::table('equipos')->insert([
+          	'numero_equipo' => 4,
+          	'actividad_id' => 3,
+          ]);
+  
+          DB::table('equipos')->insert([
+          	'numero_equipo' => 5,
+          	'actividad_id' => 3,
+          ]);
+  
+          DB::table('equipos')->insert([
+          	'numero_equipo' => 1,
+          	'actividad_id' => 4,
+          ]);
+  
+          DB::table('equipos')->insert([
+          	'numero_equipo' => 2,
+          	'actividad_id' => 4,
+          ]);
+  
+          DB::table('equipos')->insert([
+          	'numero_equipo' => 3,
+          	'actividad_id' => 4,
+          ]);
+  
+          DB::table('equipos')->insert([
+          	'numero_equipo' => 4,
+          	'actividad_id' => 4,
+          ]);
+  
+          DB::table('equipos')->insert([
+          	'numero_equipo' => 5,
+          	'actividad_id' => 4,
+          ]);
+  
+          DB::table('equipos')->insert([
+          	'numero_equipo' => 1,
+          	'actividad_id' => 5,
+          ]);
+  
+          DB::table('equipos')->insert([
+          	'numero_equipo' => 2,
+          	'actividad_id' => 5,
+          ]);
+  
+          DB::table('equipos')->insert([
+          	'numero_equipo' => 3,
+          	'actividad_id' => 5,
+          ]);
+  
+          DB::table('equipos')->insert([
+          	'numero_equipo' => 4,
+          	'actividad_id' => 5,
+          ]);
+  
+          DB::table('equipos')->insert([
+          	'numero_equipo' => 5,
+          	'actividad_id' => 5,
+          ]);
     }
 }
