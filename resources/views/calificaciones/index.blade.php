bb4ec441d3f31acdf247ad4a4b46424377596e10
Mónica <Mnica>
2017-04-01 11:22:41 -0600

    se cambio el estilo del chart

--- resources/views/calificaciones/index.blade.php
+++ resources/views/calificaciones/index.blade.php
@@ -12,53 +12,121 @@
 	<div class="content">
 
 		<div class = "chart">
-				<div id="chartContainer" style="height: 400px; width: 100%;"></div>
-				<script type="text/javascript">
-					window.onload = function () {
-						var chart = new CanvasJS.Chart("chartContainer",
-						{
-							title: {
-								text: "Tu desempeno"
-							},
-							axisX: {
-								title: "Eje X",
-								maximum: 100,
-								minimum: 0
-							},
-							axisY: {
-								title: "Eje Y",
-								maximum: 100,
-								minimum: 0
-								
-								
-							},
+				<!-- Styles -->
+				<style>
+				#chartdiv {
+					width	: 100%;
+					height	: 500px;
+				}									
+				</style>
 
-							legend: {
-								verticalAlign: "bottom",
-								horizontalAlign: "left"
-							},
-							data: [
-							{
-								type: "bubble",
-								legendText: " ",
-								showInLegend: true,
-								legendMarkerType: "circle",
-								toolTipContent: "<strong>{name}</strong> <br/> Hardcodea Eje X: {x}<br/> Hardcodea Eje Y: {y}<br/> Hardcodea Eje Z: {z}",
-								dataPoints: [
-									{ x: 78.1, y: 70.00, z: 306.77, name: "Liderazgo" },
-									{ x: 15.5, y: 82.15, z: 237.414, name: "Honestidad" },
-									{ x: 72.5, y: 90.86, z: 500.24, name: "Sex appeal" },
-								
-								]
-							}
-							]
-						});
+				<!-- Resources -->
+				<script src="https://www.amcharts.com/lib/3/amcharts.js"></script>
+				<script src="https://www.amcharts.com/lib/3/xy.js"></script>
+				<script src="https://www.amcharts.com/lib/3/plugins/export/export.min.js"></script>
+				<link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
+				<script src="https://www.amcharts.com/lib/3/themes/light.js"></script>
 
-						chart.render();
-					}
+				<!-- Chart code -->
+				<script>
+				var chart = AmCharts.makeChart( "chartdiv", {
+				"type": "xy",
+				"theme": "light",
+				"balloon":{
+				"fixedPosition":true,
+				},
+				"dataProvider": [ {
+					"y": 10,
+					"x": 14,
+					"value": 59,
+					"y2": -5,
+					"x2": -3,
+					"value2": 44
+				}, {
+					"y": 5,
+					"x": 3,
+					"value": 50,
+					"y2": -15,
+					"x2": -8,
+					"value2": 12
+				}, {
+					"y": -10,
+					"x": 8,
+					"value": 19,
+					"y2": -4,
+					"x2": 6,
+					"value2": 35
+				}, {
+					"y": -6,
+					"x": 5,
+					"value": 65,
+					"y2": -5,
+					"x2": -6,
+					"value2": 168
+				}, {
+					"y": 15,
+					"x": -4,
+					"value": 92,
+					"y2": -10,
+					"x2": -8,
+					"value2": 102
+				}, {
+					"y": 13,
+					"x": 1,
+					"value": 8,
+					"y2": -2,
+					"x2": 0,
+					"value2": 41
+				}, {
+					"y": 1,
+					"x": 6,
+					"value": 35,
+					"y2": 0,
+					"x2": -3,
+					"value2": 16
+				} ],
+				"valueAxes": [ {
+					"position": "bottom",
+					"axisAlpha": 0
+				}, {
+					"minMaxMultiplier": 1.2,
+					"axisAlpha": 0,
+					"position": "left"
+				} ],
+				"startDuration": 1.5,
+				"graphs": [ {
+					"balloonText": "x:<b>[[x]]</b> y:<b>[[y]]</b><br>value:<b>[[value]]</b>",
+					"bullet": "circle",
+					"bulletBorderAlpha": 0.2,
+					"bulletAlpha": 0.8,
+					"lineAlpha": 0,
+					"fillAlphas": 0,
+					"valueField": "value",
+					"xField": "x",
+					"yField": "y",
+					"maxBulletSize": 100
+				}, {
+					"balloonText": "x:<b>[[x]]</b> y:<b>[[y]]</b><br>value:<b>[[value]]</b>",
+					"bullet": "diamond",
+					"bulletBorderAlpha": 0.2,
+					"bulletAlpha": 0.8,
+					"lineAlpha": 0,
+					"fillAlphas": 0,
+					"valueField": "value2",
+					"xField": "x2",
+					"yField": "y2",
+					"maxBulletSize": 100
+				} ],
+				"marginLeft": 46,
+				"marginBottom": 35,
+				"export": {
+					"enabled": true
+				}
+				} );
 				</script>
-				<script src="../js/canvasjs.min.js"></script>	
-			</div>
+
+				<!-- HTML -->
+				<div id="chartdiv"></div>
 
 
 
