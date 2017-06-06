8efd3987ddf6c1858101c6ba20d12cf40c84c34d
Angel Roberto Ruiz Mendoza <angel.r.ruiz.m@gmail.com>
2017-06-06 16:12:44 -0500

    Seeds

--- database/seeds/AlumnoCRNTableSeeder.php
+++ database/seeds/AlumnoCRNTableSeeder.php
@@ -18,35 +18,22 @@ class AlumnoCRNTableSeeder extends Seeder
 
         DB::table('alumno_crn')->insert([
         	'crn_id'=>2,
-<<<<<<< HEAD
         	'alumno_id'=>2,
-=======
-        	'alumno_id'=>1,
->>>>>>> c01759f2a2621c317a5d77e1a770ba22ed143e1d
         ]);
 
         DB::table('alumno_crn')->insert([
         	'crn_id'=>3,
-<<<<<<< HEAD
         	'alumno_id'=>3,
         ]);
 
         DB::table('alumno_crn')->insert([
-            'crn_id'=>4,
-            'alumno_id'=>4,
+        	'crn_id'=>4,
+        	'alumno_id'=>4,
         ]);
 
         DB::table('alumno_crn')->insert([
-            'crn_id'=>5,
-            'alumno_id'=>5,
-=======
-        	'alumno_id'=>1,
-        ]);
-
-        DB::table('alumno_crn')->insert([
-            'crn_id'=>4,
-            'alumno_id'=>1,
->>>>>>> c01759f2a2621c317a5d77e1a770ba22ed143e1d
+        	'crn_id'=>5,
+        	'alumno_id'=>5,
         ]);
     }
 }
