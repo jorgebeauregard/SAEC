a3e04a609100349229bb51cecfbf9090814c56a4
Mónica <monica.p.m@outlook.com>
2017-11-07 09:39:54 -0600

    Se agregó profesor en seeder (Graciela)

--- database/seeds/ProfesorsTableSeeder.php
+++ database/seeds/ProfesorsTableSeeder.php
@@ -17,6 +17,14 @@ class ProfesorsTableSeeder extends Seeder
             'apellido' => 'Flores',
             'correo' => 'aflores@itesm.mx',
             'user_id' => 6,
+        ]);
+        
+        DB::table('profesors')->insert([
+        	'nomina' => 'L023456789',
+            'nombre' => 'Graciela',
+            'apellido' => 'Maldonado',
+            'correo' => 'gmc@itesm.mx',
+            'user_id' => 36,
     	]);
     }
 }
